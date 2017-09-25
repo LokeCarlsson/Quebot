dc8a73a1c333d37354395f829d345b26e7f4be62
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-25 10:36:04 +0200

    Action route refactor

--- server/src/routes/actions.js
+++ server/src/routes/actions.js
@@ -1,52 +1,41 @@
 import express from 'express'
 import mq from '../libs/things'
-import {createOrUpdateMessage} from '../libs/dbHelpers'
+import { createOrUpdateMessage } from '../libs/dbHelpers'
 import resource from '../../resource'
 import User from '../model/User'
 const router = express.Router()
 
-router.route('/actions')
-.get((req, res) => {
-  const {resources} = resource.links.actions
+router
+  .get('/actions', (req, res) => {
+    const { resources } = resource.links.actions
+    
+    const response = Object.keys(resources).reduce((array, action) => {
+      const newAction = { id: action, name: resources[action].description }
+      return [...array, newAction]
+    }, [])
 
-  const response = Object.keys(resources).reduce((array, action) => {
-    return [...array, {
-      id: action,
-      name: resources[action].description
-    }]
-  }, [])
-
-  res.status(200).send(response)
-})
-
-router.route('/actions/lcdState')
-.post((req, res) => {
-  const {student} = req.body
-  createOrUpdateMessage(req.body)
-    .then(data => {
+    res.status(200).send(response)
+  })
+  .post('/actions/lcdState', async (req, res) => {
+    const { student } = req.body
+    try {
+      const updatedMessage = await createOrUpdateMessage(req.body)
       mq.updateMessageQue()
-      return res.status(204).send()
-    })
-    .catch(error => {
-      res.status(500).send('Internal Server Error')
-      console.log(error) // Keeping for logs
-    })
-
-})
-.delete((req, res) => {
-  const {username} = req.body
-
-  User.findOneAndRemove({username})
-    .then(removedMessage => {
-      if (!removedMessage) return res.status(400).send()
-      
+      res.sendStatus(204)
+    } catch (error) {
+      res.sendStatus(500)
+    }
+  })
+  .delete('/actions/lcdState', async (req, res) => {
+    const { username } = req.body
+    try {
+      const removedMessage = await User.findOneAndRemove({ username })
+      if (!removedMessage) return res.sendStatus(400)
       mq.updateMessageQue()
-      return res.status(204).send()
-    })
-    .catch(error => {
+      res.sendStatus(204)
+    } catch (error) {
       res.status(500).send('Internal Server Error')
-      console.log(error) // Keeping for logs
-    })
-})
+    }
+  })
 
 export default router
