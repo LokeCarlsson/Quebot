10a7591a7f2333ed544af96391608ccbe0126a0f
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-25 10:40:05 +0200

    Message validation middleware refactor

--- server/src/routes/middlewares/messageValidation.js
+++ server/src/routes/middlewares/messageValidation.js
@@ -1,13 +1,6 @@
 export default (request, response, next) => {
-  const {student, username} = request.body
-
-  if (student === undefined) {
-    return next(new Error('Request must include student details'))
-  }
-
-  if (username === undefined) {
-    return next(new Error('Request must include username details'))
-  }
-
+  const { student, username } = request.body
+  if (!student) return next(new Error('Request must include student details'))
+  if (!username) return next(new Error('Request must include username details'))
   next()
 }
