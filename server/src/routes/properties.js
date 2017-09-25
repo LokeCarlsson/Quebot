854303b04d4680fda131650c49fd3ebeb4782519
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-25 11:48:14 +0200

    Refactored property route

--- server/src/routes/properties.js
+++ server/src/routes/properties.js
@@ -1,62 +1,43 @@
 import express from 'express'
 import resource from '../../resource.json'
 import User from '../model/User'
-import {createOrUpdateMessage} from '../libs/dbHelpers'
+import { createOrUpdateMessage } from '../libs/dbHelpers'
+
 const router = express.Router()
 
-router.route('/properties')
-.get((req, res) => {
-  const {resources} = resource.links.properties
-  User.find({}, '-_id message updatedAt createdAt')
-    .then(messages => {
-      const msgs = messages.map(message => {
-        return {
-          timestamp: message.updatedAt || message.createdAt,
-          message: `${message.message.firstRow} ${message.message.secondRow}`
-        }
-      })
-      return msgs
-    })
-    .then(messages => {
+router
+  .get('/properties', async (req, res) => {
+    const { resources } = resource.links.properties
+    try {
+      const messages = await User.find({}, '-_id message updatedAt createdAt')
+      const newMessages = messages.map(message => ({
+        timestamp: message.updatedAt || message.createdAt,
+        message: `${message.message.firstRow} ${message.message.secondRow}`,
+      }))
       const response = Object.keys(resources).reduce((array, resource) => {
-        if (resource === 'LCD') {
-          let prop = {
-            id: resource,
-            name: resources[resource].name,
-            values: messages
-          }
-          return [...array, prop]
-        }
-        let prop = {
+        const prop = {
           id: resource,
-          name: resources[resource].name,
+          name: resource === 'LCD' ? resources[resource].name : undefined,
+          values: messages,
         }
         return [...array, prop]
       }, [])
-      return res.status(200).json(response)
-    })
-    .catch(e => {
-      console.log(e)
-      return res.status(500).send('Intern Server error')
-    })
-})
-
-router.route('/properties/lcdState')
-.get((req, res) => {
-  User.find({}, '-_id message updatedAt createdAt')
-    .then(messages => {
-      const newArray = messages.map(message => {
-        return {
-          timestamp: message.updatedAt || message.createdAt,
-          message: `${message.message.firstRow} ${message.message.secondRow}`
-        }
-      })
-      
-      res.status(200).json(newArray)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-})
+      res.status(200).json(response)
+    } catch (error) {
+      res.sendStatus(500)
+    }
+  })
+  .get('/properties/lcdState', async (req, res) => {
+    try {
+      const messages = await User.find({}, '-_id message updatedAt createdAt')
+      const newArray = messages.map(message => ({
+        timestamp: message.updatedAt || message.createdAt,
+        message: `${message.message.firstRow} ${message.message.secondRow}`,
+      }))
+      res.json(newArray)
+    } catch (error) {
+      res.sendStatus(500)
+    }
+  })
 
 export default router
